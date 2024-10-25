70cd10ec17e78e82f2e4700c1e6ffce245817abd
Zaytsev Sergey <sergey.zaytsev@tccenter.ru>
2024-10-25 22:39:03 +0300

    hw02 added test

--- hw02/src/test/java/dao/CsvQuestionDaoTest.java
+++ hw02/src/test/java/dao/CsvQuestionDaoTest.java
@@ -1,11 +1,33 @@
 package dao;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.CsvQuestionDao;
 
 public class CsvQuestionDaoTest {
 
+    @Mock
+    private TestFileNameProvider provider;
+
+    @Mock
+    private CsvQuestionDao dao;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     void findAll() {
-    // Пока не придумал, как можно написать интеграционный тест, без поднятия контекста.
+        final var path = "classpath:questions.csv";
+
+        Mockito.when(provider.getTestFileName()).thenReturn(path);
+        final var result = dao.findAll();
+        Assertions.assertNotNull(result);
     }
 }
